d8357b69f6c5f6360b8425c29a02d931e979535b
Lynn Bradshaw <readyready15728@gmail.com>
2024-11-25 05:47:49 -0500

    Initializing LazyVim and setting up cyberdream.nvim colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "scottmckendry/cyberdream.nvim",
+    opts = {
+      transparent = true,
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "cyberdream",
+    },
+  },
+}
