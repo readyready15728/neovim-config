0440d5e9107546c02936e855c1ba97f63fb0e9c1
Lynn Bradshaw <readyready15728@gmail.com>
2024-11-25 07:37:49 -0500

    Enabling showmatch and small reordering

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,9 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.relativenumber = false
+
 vim.opt.whichwrap:append({
   ["<"] = true,
   [">"] = true,
 })
 
-vim.opt.relativenumber = false
+vim.opt.showmatch = true
+vim.opt.matchtime = 4
