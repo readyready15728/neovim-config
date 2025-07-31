a9c907305a5e40c01cb99c649124efeaf79c5a20
Lynn Bradshaw <readyready15728@gmail.com>
2025-07-31 00:11:32 -0400

    Adding custom keybinding for terminal windows to use <Esc> to enter normal mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- The default switch to normal mode in terminal windows is too clunky
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
